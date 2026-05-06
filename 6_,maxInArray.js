71c64abd42d10356e3c31bf8fb1f0f5f1de3651e
Tayyab Kamran Sami <tayyabsammy1@gmail.com>
2026-05-06 21:51:23 +0500

    feat: max in array

---
+++ 6_,maxInArray.js
@@ -0,0 +1,11 @@
+function findMax(nums) {
+    let max = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > max) {
+            max = nums[i];
+        }
+    }
+
+    return max;
+}
