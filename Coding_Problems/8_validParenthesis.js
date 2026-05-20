b011eb1ff7539a017e7b5746917d75b7d3e803a4
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2026-05-20 10:13:29 +0500

    valid paranthesis easy problem

---
+++ Coding_Problems/8_validParenthesis.js
@@ -0,0 +1,20 @@
+function isValid(s) {
+  const stack = [];
+  const map = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (let i = 0; i < map.length; i++) {
+    const char = s[i];
+    if ("{[(".includes(char)) {
+      stack.push(char);
+    } else {
+      if (stack.pop() !== map[char]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
