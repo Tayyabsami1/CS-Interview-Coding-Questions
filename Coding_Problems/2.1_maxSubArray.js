c15a9e728610c4f6a2c2aa158ec88022f70ca65a
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2026-05-24 16:48:42 +0500

    max sub array

---
+++ Coding_Problems/2.1_maxSubArray.js
@@ -0,0 +1,20 @@
+function findSubarray(nums, k) {
+  let left = 0;
+  let sum = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+
+    // ONLY ONE REMOVAL
+    if (sum > k) {
+      sum -= nums[left];
+      left++;
+    }
+
+    if (sum === k) {
+      return nums.slice(left, right + 1);
+    }
+  }
+
+  return null;
+}
