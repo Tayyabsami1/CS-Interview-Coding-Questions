566d269923bce8c30a08194af69a74983d4c977a
tayyabsami1 <tayyabsammy1@gmail.com>
2026-08-10 19:09:36 +0500

    aws cloud architect course initialize

--- Coding_Problems/JS/1_validAnagram.js
+++ Coding_Problems/JS/1_validAnagram.js
@@ -4,9 +4,8 @@ function isValidAnagram(s, t) {
   s = normalize(s);
   t = normalize(t);
   const charCount = {};
-  for (let char of s) {
-    charCount[char] = (charCount[char] || 0) + 1;
-  }
+  for (let char of s) charCount[char] = (charCount[char] || 0) + 1;
+
   for (let char of t) {
     if (!charCount[char]) return false;
     charCount[char]--;
